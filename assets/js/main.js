8799602852025ce81d0a24ba285eed6c2e641f40
Micael Santana <109448500+iSantanaaH@users.noreply.github.com>
2022-08-29 22:38:14 -0400

    Commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const txtName = document.getElementById('txtName');
+const txtEmail = document.getElementById('txtEmail');
+const txtLevel = document.getElementById('txtLevel');
+const txtCharacter = document.getElementById('txtCharacter');
+
+
+
+
+
